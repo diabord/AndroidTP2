474b7e97a04092592725e52a797f7911cf7918a8
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-11-23 15:52:45 +0100

    fait joujou

--- app/src/main/java/com/example/td2_jin/tasklist/TaskListAdapter.kt
+++ app/src/main/java/com/example/td2_jin/tasklist/TaskListAdapter.kt
@@ -1,29 +1,37 @@
 package com.example.td2_jin.tasklist
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.td2_jin.R
 
 class TaskListAdapter(private val taskList: List<String>) : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): TaskListAdapter.TaskViewHolder {
-        TODO("Not yet implemented")
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return taskList.size;
     }
 
+
+
     override fun onBindViewHolder(holder: TaskListAdapter.TaskViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(taskList[position])
     }
 
     inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(taskTitle: String) {
             itemView.apply { // `apply {}` permet d'éviter de répéter `itemView.*`
                 // TODO: afficher les données et attacher les listeners aux différentes vues de notre [itemView]
+                val textView =  findViewById<TextView>(R.id.task_title)
+                textView.text = taskTitle
             }
         }
     }
