b818bf92f8aaa0db05537f210263967d59f99310
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-11-23 16:35:14 +0100

    add button add

--- app/src/main/java/com/example/td2_jin/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/td2_jin/tasklist/TaskListFragment.kt
@@ -8,10 +8,12 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.td2_jin.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
 
 
 class TaskListFragment : Fragment() {
-    private val taskList = listOf(
+    private val taskList = mutableListOf<Task>(
         Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3"))
@@ -29,5 +31,10 @@ class TaskListFragment : Fragment() {
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = TaskListAdapter(taskList)
+
+        view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
+            val task = Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
+            taskList.add(task);
+        }
     }
 }
