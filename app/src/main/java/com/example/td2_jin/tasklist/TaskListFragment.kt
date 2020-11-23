c007c8308d82591c3ad76f8892c233d30adbeebc
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-11-23 17:10:49 +0100

    button to add task

--- app/src/main/java/com/example/td2_jin/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/td2_jin/tasklist/TaskListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,7 +14,7 @@ import java.util.*
 
 
 class TaskListFragment : Fragment() {
-    private val taskList = mutableListOf<Task>(
+    private val taskList = mutableListOf(
         Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3"))
@@ -35,6 +36,8 @@ class TaskListFragment : Fragment() {
         view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
             val task = Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
             taskList.add(task);
+
+            (recyclerView.adapter as TaskListAdapter).notifyItemInserted(taskList.size - 1);
         }
     }
 }
