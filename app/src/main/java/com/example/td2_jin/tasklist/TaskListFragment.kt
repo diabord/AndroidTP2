25a82bfec8cf66db033dfde90b704d818363da4f
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-11-23 15:05:02 +0100

    update TakListFragment

--- app/src/main/java/com/example/td2_jin/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/td2_jin/tasklist/TaskListFragment.kt
@@ -1,4 +1,19 @@
 package com.example.td2_jin.tasklist
 
-class TaskListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.td2_jin.R
+
+class TaskListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
+        return rootView
+    }
 }
